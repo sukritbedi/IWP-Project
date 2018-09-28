d7e234c26e9b8cf1112d6c1397e17501b23997ef
Sukrit Bedi <sukritbedi1@gmail.com>
2018-09-28 19:49:49 +0530

    Regular Upload 13
    Finalized the movie view design in the main user page

--- main/angJs.js
+++ main/angJs.js
@@ -1,5 +1,16 @@
 var app = angular.module("myApp",[]);
 
+          app.filter("age", function(){
+              return function (goit){
+                  switch(goit){
+                      case "1":
+                        return "A";
+                      case "2":
+                        return "U/A"
+                  }
+              }
+          })
+
           app.controller("main", function($scope,$http){
             $http.get('fetchMovie.php')
             .then(function(response) {
