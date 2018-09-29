0300ea2631810bd7aebdcf8abc58cdc634c894da
Sukrit Bedi <sukritbedi1@gmail.com>
2018-09-29 23:24:31 +0530

    Regular Upload 14
    Created seperate files and added ng-include feature for all the pages

--- main/angJs.js
+++ main/angJs.js
@@ -3,6 +3,8 @@ var app = angular.module("myApp",[]);
           app.filter("age", function(){
               return function (goit){
                   switch(goit){
+                      case "0":
+                        return "U";
                       case "1":
                         return "A";
                       case "2":
@@ -11,10 +13,23 @@ var app = angular.module("myApp",[]);
               }
           })
 
-          app.controller("main", function($scope,$http){
+          app.controller("movie_all", function($scope,$http,$rootScope){
+            $rootScope.main = true;
+
             $http.get('fetchMovie.php')
             .then(function(response) {
               $scope.movies = response.data;
               console.log($scope.movies);
             })
-          });
+
+            $scope.moviebook1 = function(mov_id) {
+              $rootScope.main = false;
+              $rootScope.moviesel = true;
+              $rootScope.mov_id = mov_id;
+            }
+          })
+
+          app.controller("movie_sel", function($scope, $rootScope, $http) {
+
+          })
+          ;
